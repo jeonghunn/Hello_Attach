b7d7792c35a982ce63f24ad9bc331295f0d068c4
Junghoon Lee <jeonghunn1@gmail.com>
2019-09-28 02:34:00 +0900

    feat: Allow creating png thumbnail

--- controller/attach.class.php
+++ controller/attach.class.php
@@ -51,7 +51,7 @@ $all_result = true;
                 $upload_result = move_uploaded_file($_FILES['uploadedfile']['tmp_name'][$i], $target_path);
                 $thumbnail_file = "";
                 $thumbnail_file = $thumbnail_path . $img_name;
-                if ($extension == "jpg" || $extension == "jpeg") {
+                if ($kind == "image" ) {
                     Thumbnail::create($target_path,
                         120, 120,
                         SCALE_EXACT_FIT,
